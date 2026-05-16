38b046ae463a6baa8bcda866855fc350b385ddcc
Soumo Saha <soumosaha4327@gmail.com>
2026-05-16 17:47:18 +0530

    config fix

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'grabmyfiles' // must match GitHub repo name exactly
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/upload',
-        destination: 'http://localhost:8080/upload',
-      },
-      {
-        source: '/api/download/:port',
-        destination: 'http://localhost:8080/download/:port',
-      },
-    ];
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
 }
 
 module.exports = nextConfig
